41a13444fce6e102c53439e1387cb1b53e62394b
John Doe <johndoe@example.com>
2023-05-31 15:14:18 +0200

    structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./style.css";
 
+import PrersonForm from "./components/PersonForm";
+import PersonList from "./components/PersonList";
+import Stats from "./components/Stats";
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Technologie Front-End- Kolokwium</h1>
+      <p>Karolina Pomian</p>
+      <PrersonForm />
+      <PersonList />
+      <Stats />
     </div>
   );
 }
